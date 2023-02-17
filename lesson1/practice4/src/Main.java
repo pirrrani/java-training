5e0e499befe55b69b0ee182f8b9917bb0f4f89c0
pirrrani <pirrrani666@mail.ru>
2023-02-17 07:45:01 +0300

    lesson 1, task 4

---
+++ lesson1/practice4/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите длительность вклада в месяцах:");
+        int m = (int)in.nextInt();
+        System.out.print("Введите месячный процент по вкладу:");
+        float p = (float)in.nextFloat();
+        System.out.print("Введите начальную сумму:");
+        int s = (int)in.nextInt();
+        System.out.println();
+
+        float D = s * p / 100;
+        System.out.println("Доход в месяц:" + D);
+        int i = 0;
+        while(i < m){
+            s += D;
+            i += 1;
+        }
+        System.out.print("Итоговая сумма:" + s);
+    }
+}
